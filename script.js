bc3895cf6657f2d9e097fecfbca2c7ec5bc6d1ce
omarmohamedismail <101266901+omarmohamedismail@users.noreply.github.com>
2024-08-14 03:12:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+/*<script>*/
+window.addEventListener("scroll", function () {
+    const header = document.querySelector("header");
+    const navLinks = document.querySelectorAll("navbar a");
+  
+    if (window.scrollY > 50) {
+      header.style.backgroundColor = "#333";
+      navLinks.forEach((link) => {
+        link.style.color = "#fff";
+      });
+    } else {
+      header.style.backgroundColor = "transparent";
+      navLinks.forEach((link) => {
+        link.style.color = "#fff";
+      });
+    }
+  });
+
+
+
+  /*</script>
+
+
+   <script>*/
+  document.querySelectorAll("nav a").forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+  
+      const targetId = this.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
+  
+      window.scrollTo({
+        top: targetElement.offsetTop - 90, // Adjust based on your header height
+        behavior: "smooth"
+      });
+    });
+  });
+  
+  /*</script>*/
+  
